b3822aca440233813ed2d3ee397daee3a7e5ea77
Leonardo Cesar <guy.fawkes.05111605.e@gmail.com>
2018-05-27 14:33:15 -0300

    Responding to user input

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -8,12 +8,27 @@ public class Hacker : MonoBehaviour
 	// Use this for initialization
 	void Start() 
 	{
-		
-	}
-	
-	// Update is called once per frame
-	void Update() 
-	{
-		
-	}
+        ShowMainMenu("Hello Nagi");
+    }
+
+    private void ShowMainMenu(string greeting)
+    {
+        Terminal.ClearScreen();
+
+        Terminal.WriteLine(greeting);
+
+        Terminal.WriteLine("Welcome to the Anonymous entrance exam");
+        Terminal.WriteLine("Select below your entrance level\n");
+
+        Terminal.WriteLine("Press 1 for the Initiate level exam");
+        Terminal.WriteLine("Press 2 for the Mid level exam");
+        Terminal.WriteLine("Press 3 for the Expert level exam\n");
+
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+    private void OnUserInput(string input)
+    {
+        Debug.Log("User typed " + input);
+    }
 }
